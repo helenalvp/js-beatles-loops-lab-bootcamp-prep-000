70425e306e135b5fcd7ab7ffa53b4ac45bda32e8
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-08-26 13:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 // add solution here
 function theBeatlesPlay(arrayOfMusicians,arrayOfInstruments) {
-var array=[];
+  var array=[];
   for (let i = 0; i < 4; i++) {
       array.push(`${arrayOfMusicians.shift()} plays ${arrayOfInstruments.shift()}`);
   }
-return array;
+  return array;
 }
 
 function johnLennonFacts(arrayofJohnLennonFacts) {
-var array = [];
-let length = arrayofJohnLennonFacts.length;
- while (length > 0) {
-  array.push(`${arrayofJohnLennonFacts.shift()}!!!`)
-   --length;
- }
-return array;
+  var array = [];
+  let length = arrayofJohnLennonFacts.length;
+  while (length > 0) {
+    array.push(`${arrayofJohnLennonFacts.shift()}!!!`)
+     --length;
+  }
+  return array;
 }
 
 function iLoveTheBeatles(number) {
-  * The function should create a variable that stores an empty array.
-* Implement a do-while loop inside the function that adds `"I love the
-  Beatles!"` to the empty array.
-* The loop should then increment the number passed in as a parameter. The
+// The function should create a variable that stores an empty array.
+
+/* Implement a do-while loop inside the function that adds `"I love the
+  Beatles!"` to the empty array. */
+  
+/* The loop should then increment the number passed in as a parameter. The
   condition of the loop should check to see that the parameter number is
   less than `15`. It should contain a number of strings that represents the
-  difference between the input number and `15`.
-* Return the array with the strings `"I love the Beatles!"`.
+  difference between the input number and `15`. */
+// Return the array with the strings `"I love the Beatles!"`.
 }
